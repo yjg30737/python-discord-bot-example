36300a62800e75da979572c252959f6425ec74b3
Jung Gyu Yoon <yjg30737@gmail.com>
2023-01-29 13:43:34 +0900

    First commit from local

---
+++ example_bot.py
@@ -0,0 +1,24 @@
+import discord
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        # plain message return (not only to you, server including this bot in general)
+        print(f'Message from {message.author}: {message.content}')
+
+        # don't answer on your own message
+        if message.author == client.user:
+            return
+
+        # respond properly when the message starts with hello
+        if message.content.startswith('Hello'):
+            await message.channel.send(f'Hello {message.author}! I am Discord Bot {self.user}, glad to meet you 🙂')
+
+
+intents = discord.Intents.default()
+
+client = MyClient(intents=intents)
+client.run('token')
